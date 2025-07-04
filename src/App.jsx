b6e45b6177a6b1ecf51d4d57c179973d1fe71a76
hibaaahhh <hibakalathil02@gmail.com>
2025-07-04 12:32:40 +0530

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Products from "./components/Products";
 import Login from "./pages/Login";
+
+const PrivateRoute = ({ children }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  return user?.user_id ? children : <Navigate to="/login" replace />;
+};
 
 function App() {
   return (
     <div className="px-4 lg:px-20 min-h-screen">
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/products" element={<Products />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <PrivateRoute>
+              <Products />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
